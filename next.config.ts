ecac0733a4c506dbe5679e02b01886dddfb8df21
Rayyan Youssef Kombargi <166548990+rayankombargi@users.noreply.github.com>
2025-05-27 18:40:54 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/pokemon',
+        destination: '<PLEASE_REPLACE_IT_WITH_YOUR_BACKEND_URL>/Prod/pokemon',
+      },
+      {
+        source: '/api/pokemon/:id',
+        destination: '<PLEASE_REPLACE_IT_WITH_YOUR_BACKEND_URL>/Prod/pokemon/:id',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
---
+++ src/model/pokemon.ts
@@ -0,0 +1,19 @@
+export interface Pokemon {
+   "pokemonNumber": 7,
+   "attack": 48,
+   "defense": 65,
+   "devolution": "",
+   "evolution": "Wartortle",
+   "evolutionFamily": [
+       "Squirtle",
+       "Wartortle",
+       "Blastoise"
+   ],
+   "healthPoints": 44,
+   "mainImage": "https://pokemon-aub-awe-workshop.s3.eu-west-2.amazonaws.com/7/mainImage.png",
+   "pokemonName": "Squirtle",
+   "pokemonType": [
+       "Water"
+   ],
+   "speed": 43
+}
